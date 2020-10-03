975c182a81cd59a91b64f6cdbc1420d8a5283793
Peng Du <rick.peng.du@gmail.com>
2020-10-03 16:50:26 -0700

    initial commit

---
+++ src/main/java/Problem.java
@@ -0,0 +1,4 @@
+public class Problem {
+    public static void reverseArray(final int[] input) {
+    }
+}
---
+++ src/test/java/ProblemTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ProblemTest {
+    @Test
+    public void test() {
+    }
+}
