326bb3ff47107fa7b54ac73851434b9d2bb9caa5
Peng Du <rick.peng.du@gmail.com>
2020-10-03 17:02:35 -0700

    add tests

--- src/test/java/ProblemTest.java
+++ src/test/java/ProblemTest.java
@@ -1,8 +1,39 @@
+import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.assertArrayEquals;
 
 public class ProblemTest {
     @Test
     public void test() {
+        int inputs[][] = {
+                {},
+                {1},
+                {-2, 2},
+                {3, -3},
+                {1, 2, 3},
+                {3, 2, 1, 0},
+                {-4, -4, -4},
+                {-1, 0, 9, 3, 2}};
+
+        int expected[][] = {
+                {},
+                {1},
+                {2, -2},
+                {-3, 3},
+                {3, 2, 1},
+                {0, 1, 2, 3},
+                {-4, -4, -4},
+                {2, 3, 9, 0, -1}};
+
+        for (int i = 0; i < inputs.length; i++) {
+            Problem.reverseArray(inputs[i]);
+            try {
+                assertArrayEquals(expected[i], inputs[i]);
+            } catch (AssertionError e) {
+                System.out.println("test case " + i + " failed: " + e);
+                Assert.fail();
+            }
+        }
     }
 }
