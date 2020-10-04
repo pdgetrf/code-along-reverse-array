c19bbc55389a35d385d716eadddc6f7d40ad6399
Peng Du <rick.peng.du@gmail.com>
2020-10-03 17:09:28 -0700

    first version

--- src/main/java/Problem.java
+++ src/main/java/Problem.java
@@ -1,4 +1,13 @@
 public class Problem {
     public static void reverseArray(final int[] input) {
+        if (input == null) {
+            return;
+        }
+
+        for (int i = 0; i < input.length / 2; i++) {
+            int tmp = input[i];
+            input[i] = input[input.length - 1 - i];
+            input[input.length - 1 - i] = tmp;
+        }
     }
 }
